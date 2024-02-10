ef8d66cd4909946c9f2c9f3f35e604417c602cb8
10b_Kövecses Levente <93580508+Kavitss@users.noreply.github.com>
2024-02-10 11:08:19 +0100

    Add files via upload

---
+++ ikt5/uj_phpk/login.php
@@ -0,0 +1,76 @@
+<?php
+    session_destroy();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+
+    if(isset($_POST["register"])){
+        header("Location: register.php");
+    }
+    include("database_connection.php");
+    if(isset($_POST["login"])){
+        
+
+        if(!empty($_POST["username"] && !empty($_POST["password"]))){
+            $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+
+            $sql = "SELECT * FROM users WHERE username = '" . $username . "';";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_assoc($result);
+        
+            if(mysqli_num_rows($result) > 0){
+                $filterd_pw = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+                if(password_verify($filterd_pw, $user["password"])){
+                    session_start();
+                    $_SESSION["username"] = $username;
+                    $_SESSION["email"] = $user["email"];
+                    $_SESSION["location"] = $user["location"];
+                    $_SESSION["phone_number"] = $user["phone_number"];
+                    $_SESSION["full_name"] = $user["full_name"];
+                    $_SESSION["cart"] = [];
+                    $_SESSION["choosen_theme_id"] = [];
+                    header("Location: index.php");
+                }
+                else{
+                    echo "Hibás jelszó! Kérem, próbálja újra!";
+                }
+            }
+            else{
+                echo "Felhasználó nem található!";
+            }
+
+
+
+            // $_SESSION["username"] = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+            // $_SESSION["password"] = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+            // echo $_SESSION["username"];
+            // echo $_SESSION["password"];
+            // header("location: index.php");
+        }
+        else{
+            echo "<h1>Kérem tötse ki mindkettő mezőt!<h1>";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>login</h1>
+    <form action="login.php" method="post">
+        <label>username:</label><br>
+        <input type="text" name="username"><br>
+        
+        <label>password:</label><br>
+        <input type="password" name="password"><br>
+        
+        <input type="submit" name="login" value="Log in">
+        <input type="submit" name="register" value="Register">
+    </form>
+</body>
+</html>
---
+++ ikt5/uj_phpk/register.php
@@ -0,0 +1,86 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>register</h1>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <label>username:</label><br>
+        <input type="text" name="username"><br>
+        
+        <label>password:</label><br>
+        <input type="password" name="password"><br>
+        
+        <label>full name:</label><br>
+        <input type="text" name="full_name"><br>
+        
+        <label>email:</label><br>
+        <input type="email" name="email"><br>
+      
+        <label>location:</label><br>
+        <input type="text" name="location"><br>
+       
+        <label>telephone:</label><br>
+        <input type="tel" name="telephone"><br>
+
+        <input type="submit" name="register" value="register">
+        <input type="submit" name="login" value="Log in">
+    </form>
+</body>
+</html>
+
+<?php
+    if(isset($_POST["login"])){
+        header("Location: login.php");
+    }
+
+    include("database_connection.php");
+    if(isset($_POST["register"])){
+
+        if(!empty($_POST["username"]) && !empty($_POST["password"]) && !empty($_POST["full_name"]) && !empty($_POST["email"]) && !empty($_POST["location"]) && !empty($_POST["telephone"])){
+
+            $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+
+            $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            $full_name = filter_input(INPUT_POST, "full_name", FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+            $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+            $location = filter_input(INPUT_POST, "location", FILTER_SANITIZE_SPECIAL_CHARS);
+            $phone_number = filter_input(INPUT_POST, "telephone", FILTER_SANITIZE_NUMBER_INT);
+            // echo $phone_number;
+
+            
+            $sql = "INSERT INTO users (username, password, location, phone_number, email, full_name)
+                    VALUES ('$username', '$hashed_password', '$location', '$phone_number', '$email', '$full_name')";
+        
+            try{
+                mysqli_query($conn, $sql);
+                session_start();
+                $_SESSION["username"] = $username;
+                $_SESSION["password"] = $hashed_password;
+                $_SESSION["full_name"] = $full_name;
+                $_SESSION["email"] = $email;
+                $_SESSION["location"] = $location;
+                $_SESSION["phone_number"] = $phone_number;
+                $_SESSION["cart"] = [];
+                header("Location: index.php");
+            }
+            catch(mysqli_sql_exception){
+                echo "Ezzel a felhasználónév/email címmel már rendelkezik fiók az oldalon";
+            }
+        }
+        else{
+            echo "<h1>Kérem tötse ki az összes mezőt!<h1>";
+        }
+    }
+?>
