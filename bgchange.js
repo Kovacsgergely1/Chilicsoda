c9c02899e40952016be9d50db429b879931ce3e2
10b_Kovács Gergely <93580926+Kovacsgergely1@users.noreply.github.com>
2024-03-24 11:20:37 +0100

    Delete bgchange.js

--- bgchange.js
+++
@@ -1,6 +0,0 @@
-function changeBackgroundColor() {
-    var welcome = document.getElementById('welcome');
-    welcome.style.backgroundColor = '#d10606'
-}
-
-setTimeout(changeBackgroundColor, 5500);
